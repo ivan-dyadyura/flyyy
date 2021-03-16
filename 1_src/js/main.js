7c48b356a765c9e6ec3ef8309a3c0feab02f4898
ivan-dyadyura <miiv226@list.ru>
2021-03-16 20:05:00 +0800

    Education center

--- 1_src/js/main.js
+++ 1_src/js/main.js
@@ -1,3 +1,21 @@
 import accordion from './components/accordion'
+import {Swiper, Navigation, Scrollbar} from 'swiper'
 
 accordion('.js-accordion', '.js-accordion__btn', '.js-accordion__content', 'js-accordion__content--active', 'franchise-start__btn--active')
+
+Swiper.use([Navigation, Scrollbar])
+
+new Swiper('.education-center__slider', {
+	slidesPerView: 'auto',
+	spaceBetween: 32,
+	navigation: {
+		nextEl: '.education-center__nav--next',
+		prevEl: '.education-center__nav--prev',
+	},
+	scrollbar: {
+		el: '.swiper-scrollbar',
+		draggable: true,
+		dragSize: '120',
+		
+	}
+})
